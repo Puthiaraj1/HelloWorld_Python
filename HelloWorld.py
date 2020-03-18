026c45a030dfc9cbc0639903f9a0e103afc15df2
Puthiaraj Anbalagan <puthiarajanbalagan@Puthiarajs-MacBook-Pro.local>
2020-03-18 15:59:35 -0400

    Initial commit

---
+++ HelloWorld.py
@@ -0,0 +1,13 @@
+print('Hello World!')
+print(1 + 2)
+print( 7 * 6)
+print()
+print("The End")
+print("Python's Strings are easy to use")
+print('We can even include "quotes" in Strings')
+print("Hello " + " World" )
+greeting = "Hello"
+name = "Raj"
+print(greeting+name)
+#if we want a space , we can add that too
+print(greeting+' '+name)
